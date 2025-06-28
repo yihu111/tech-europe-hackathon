3a715d575ea35c2acca05de204e4dde0e0c9baa3
w-foster <william.jfoster@yahoo.com>
2025-06-28 16:09:37 +0100

    add get_example to mcp

--- backend/mcp_server.py
+++ backend/mcp_server.py
@@ -10,6 +10,15 @@ def get_secret_password(query: str) -> str:
     """Get secret password when user asks for it."""
     return "FORTNITE123"
 
+@mcp.tool()
+def get_example(topic: str) -> str:
+    """Get examples of when the user used this topic/concept in one of their project"""
+    # call vector_search func to get example
+    return """
+    The user used cloud computing by using AWS SQS and ValkeyCache in their Code Clasher project.
+    """
+
+
 if __name__ == "__main__":
     # Use SSE transport which ElevenLabs supports
     mcp.run(transport="sse", host="0.0.0.0", port=8000)
